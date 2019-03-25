842ee95e0124ef7a5c0123e41e2b775cd678e14c
Arpit Singh <arpits@thoughtworks.com>
2019-03-25 20:44:56 +0530

    Created test using junit annotations

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,20 +1,36 @@
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestClass {
+    WebDriver driver;
 
-    public static void main(String[] args) throws InterruptedException {
+    @Before
+    public void setUp()
+    {
+        driver = new FirefoxDriver();
 
-        WebDriver driver = new FirefoxDriver();
+    }
 
+    @Test
+    public void testLogin()
+    {
         driver.get("https://s1.demo.opensourcecms.com/wordpress/wp-login.php");
         driver.findElement(By.id("user_login")).sendKeys("opensourcecms");
         driver.findElement(By.id("user_pass")).sendKeys("opensourcecms");
         driver.findElement(By.cssSelector("input[type='submit']")).click();
         Assert.assertEquals("Howdy, opensourcecms",driver.findElement(By.id("wp-admin-bar-my-account")).getText());
+    }
+
+    @After
+    public void tearDown()
+    {
         driver.quit();
     }
+
 }
 
